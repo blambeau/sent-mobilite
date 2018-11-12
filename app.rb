4239c72b87d8ae4a0c3f46763cb28d9a0c53add3
Bernard Lambeau <blambeau@gmail.com>
2018-11-12 13:57:31 +0100

    Make sure /sondage is recognized too, redirect to /sondage/

--- app.rb
+++ app.rb
@@ -23,6 +23,10 @@ get '/' do
   redirect "/sondage/"
 end
 
+get '/sondage' do
+  redirect "/sondage/"
+end
+
 get '/sondage/' do
   send_file File.join(settings.public_folder, 'sondage/index.html')
 end
